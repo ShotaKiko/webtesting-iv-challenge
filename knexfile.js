6a8eae6e6fae96672080d1c4e2c90eac1b282c02
ShotaKiko <Shota.Kikozashvili@gmail.com>
2019-06-28 13:37:22 -0500

    added db for dev and testing

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './data/inn.sqlite3'
+      filename: './data/inn.db3'
     },
     useNullAsDefault: true,
     migrations: {
