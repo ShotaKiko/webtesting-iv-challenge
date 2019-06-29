2c04b58c91172747aa021d968ce5959486d7c267
ShotaKiko <Shota.Kikozashvili@gmail.com>
2019-06-28 23:24:39 -0500

    added tests for server get

--- api/server.spec.js
+++ api/server.spec.js
@@ -5,4 +5,13 @@ describe('server.js', () => {
     it('should set the test environment', () => {
         expect(process.env.DB_ENV).toBe('testing')
     })
+
+    describe('GET /', () => {
+        it('should return 200', async () => {
+            const res = await request(server).get('/')
+            expect(res.status).toBe(200)
+            expect(res.type).toBe('application/json')
+            expect(res.body).toEqual({ message:"Server is live" })
+        })
+    })
 })
